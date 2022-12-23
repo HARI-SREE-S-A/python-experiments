dbdf25f738e12df8f9b4a1ab22d8fead5d764b99
harvis <getharisree@outlook.com>
2022-12-23 17:57:33 +0530

    Create expense_checker.py

---
+++ expense_checker.py
@@ -0,0 +1,6 @@
+milage = int(input("enter the milage of bike"))
+petrolprice = int(input("enter the petrol price in yur city"))
+dist = int(input("enter the distance"))
+fuel_con = (dist /milage)
+
+print("expense for the travel is ",fuel_con*petrolprice)
