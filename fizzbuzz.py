73e7134328b66cdb326725a134a702888f993f7b
HARISREE SURESH <getharisree@outlook.com>
2022-12-27 19:47:18 +0530

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+number = list(range(1,25))
+for i in range(len(number)):
+    if number[i]%4 ==0 and number[i]%6 == 0:
+        print("fizzbuzz")
+    elif number[i]%4 == 0:
+        print("fizz")
+
+    elif number[i]%6 == 0:
+        print("buzz")
+
+    else:
+        print(number[i])
