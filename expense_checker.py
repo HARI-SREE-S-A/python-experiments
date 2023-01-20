bff4dbbd28fa4965d7100fccdf99ee2b246aad1f
HARISREE SURESH <getharisree@outlook.com>
2023-01-20 20:00:46 +0530

    Update expense_checker.py

--- expense_checker.py
+++ expense_checker.py
@@ -3,6 +3,4 @@ petrolprice = int(input("enter the petrol price in yur city"))
 dist = int(input("enter the distance"))
 fuel_con = (dist /milage)
 
-
-
 print("expense for the travel is ",fuel_con*petrolprice)
