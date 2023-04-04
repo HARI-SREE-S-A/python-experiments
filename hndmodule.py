0f3e5269c3899d194ba0bfc52f9333902bbb2135
HARISREE SURESH <getharisree@outlook.com>
2023-04-04 19:43:59 +0530

    Update hndmodule.py

--- hndmodule.py
+++ hndmodule.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 
+
 class handDetector():
     def __init__(self, mode=False, maxHands=2, model_complexity=1, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
